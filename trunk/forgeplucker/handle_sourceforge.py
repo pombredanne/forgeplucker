64c21d5a6fd9aa94904c53e6f748d44fbef8fb40
ptd <ptd@7e9734f2-5825-4a1b-abe6-0096d909c21f>
2009-11-07 20:58:48 +0000

    Updated handle_sourceforge.py and added basic.py, which demos it
    git-svn-id: svn+ssh://svn.gna.org/svn/forgeplucker@26 7e9734f2-5825-4a1b-abe6-0096d909c21f

--- trunk/forgeplucker/handle_sourceforge.py
+++ trunk/forgeplucker/handle_sourceforge.py
@@ -3,6 +3,8 @@ Handler class for SourceForge.
 
 This doesn't work yet.  It's a stub.
 """
+import re
+
 from htmlscrape import *
 from generic import *
 
@@ -11,26 +13,66 @@ class SourceForge(GenericForge):
 The SourceForge handler provides bug-plucking machinery for the SourceForge
 site.
 
-This code does not capture custom trackers.
+This code will capture custom trackers.
 """
+    class Tracker():
+        """Generic class for a SourceForge tracker"""
+        def __init__(self,forge,atid):
+            self.forge = forge
+            self.group_id = forge.group_id
+            self.atid = atid
+            print 'atid:',self.atid
+        def getbugids(self):
+            forge = self.forge
+            ids = []
+            url = r'/tracker/?group_id='+self.group_id+r'&limit=100&atid='+self.atid
+            while True:
+                page = forge.fetch(url,"Bug list")
+                print url
+                ms = re.findall(r'/tracker/\?func=detail&aid=([0-9]*)&group_id='+self.group_id+r'&atid='+self.atid,page)
+                if ms:
+                    ids += ms
+                else:
+                    if ids:
+                        self.forge.error("Can't get ALL (we got some) bugids in " + str(self))
+                    else:
+                        self.forge.error("Can't get bugids in " + str(self))
+                m = re.search(r'href="([^ <>]*?)">Next',page)
+                if m:
+                    url = re.sub(r'&amp;',r'&',m.group(1))
+                else:
+                    return ids #There is no 'Next' button we have all bug ids
     def __init__(self, host, project_name):
-        GenericForge.__init__(self, host, project_name);
+        GenericForge.__init__(self, host, project_name)
+        self.develop_page = self.fetch(self.project_page() + 'develop',"Develop page")
+        self.group_id = self.getgroup_id()
         self.trackers = [
             #SourceForge.BugTracker(self),
             #SourceForge.SupportTracker(self),
             #SourceForge.PatchTracker(self),
             #SourceForge.FeatureTracker(self),
             ]
-    def project_page(self, project):
-        return "projects/%s/" % (project,)
+    def project_page(self):
+        return "projects/%s/" % (self.project_name,)
+    def getgroup_id(self):
+        """Never run this, use self.group_id instead.
+           This is only called in __init__"""
+        page = self.develop_page
+        m = re.search(r'/tracker/\?group_id=([0-9]*)',page)
+        if m:
+            return m.group(1)
+        else:
+            self.error("Unable to get group_id")
+    def getbugatid(self):
+        page = self.develop_page
+        m = re.search(r'/tracker/\?group_id='+self.group_id+'&amp;atid=([0-9]*)">Bugs',page)
+        if m:
+            return m.group(1)
+        else:
+            self.error("Unable to get bugatid")
+    def getbugTracker(self):
+        return self.Tracker(self,self.getbugatid())
     def login(self, username, password):
-        #mainpage =  "projects/%s/" % self.project_name
-        #m = re.search(r'\?group_id=([0-9]*)',
-        #              self.fetch(mainpage, "Project Page"))
-        #if m:
-        #    self.project_id = int(m.group(1))
-        #else:
-        #    self.error("can't find a project ID for %s" % self.project_name)
         GenericForge.login(self, {
             'form_loginname':username,
             'form_pw':password,
