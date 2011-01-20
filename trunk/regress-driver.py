6ae5b37dea1ee58a6ab5617ce726ff55933ab763
oberger <oberger@7e9734f2-5825-4a1b-abe6-0096d909c21f>
2011-01-20 11:23:14 +0000

    Fails nicely when config option is missing in .cfg file
    git-svn-id: svn+ssh://svn.gna.org/svn/forgeplucker@221 7e9734f2-5825-4a1b-abe6-0096d909c21f

--- trunk/regress-driver.py
+++ trunk/regress-driver.py
@@ -79,10 +79,13 @@ def runtest(testenv, output, skip_failing = False):
         if not config_parser :
             config_parser = ConfigParser.RawConfigParser(testenv)
         config_parser.read(config_file)
-        username = config_parser.get('parameters', 'username')
-        format = config_parser.get('parameters', 'format')
-        options = config_parser.get('parameters', 'options')
-        test = config_parser.get('parameters', 'project')
+        try:
+            username = config_parser.get('parameters', 'username')
+            format = config_parser.get('parameters', 'format')
+            options = config_parser.get('parameters', 'options')
+            test = config_parser.get('parameters', 'project')
+        except ConfigParser.NoOptionError, e:
+            msg('Warning : ' + config_file + ': ' + e.message)
 
     cmd = testcmd + ' -v ' + str(verbose) + ' '
     if username != None:
